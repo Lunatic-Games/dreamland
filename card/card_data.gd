4ae61f92c2cf188363e52dde0d1ab53f8e7c0585
JoeZlonicky <joe.zlonicky@gmail.com>
2022-08-31 17:20:13 -0700

    Card data resource

---
+++ card/card_data.gd
@@ -0,0 +1,11 @@
+extends Resource
+
+class_name CardData
+
+export (String) var display_name
+export (Texture) var texture
+
+export (int) var athletics_modifier
+export (int) var charisma_modifier
+export (int) var intelligence_modifier
+export (int) var strength_modifier
